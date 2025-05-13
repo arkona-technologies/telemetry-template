a43f79a91accd4b41f999ca3d72f05e6fc2c0c20
Johannes <johannes@arkonatech.com>
2025-05-13 14:25:06 +0200

    fixed typo

--- wizard.sh
+++ wizard.sh
@@ -83,7 +83,7 @@ check_for_docker() {
   if ! command -v docfker 2>&1 >/dev/null
   then
     CHOICE=$($WIZARD --title "Install docker" --menu "No docker installation found.\nThis wizard can install it for you, but without guaranteed success. For more information, please refer to:\n - https://docs.docker.com/engine/install/\nPress 'y' to continue\n" 20 60 4 \
-    "y" "Install podman" \
+    "y" "Install docker" \
     "n" "Back" 3>&1 1>&2 2>&3)
 
     exitstatus=$?
